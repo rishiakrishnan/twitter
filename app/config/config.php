2a50c60f0d07ae3c1bf2f1759768bf14fda97f4b
rishiakrishnan <rishiakrishnan@gmail.com>
2026-03-06 15:35:35 +0530

    second

--- app/config/config.php
+++ app/config/config.php
@@ -3,14 +3,16 @@ ob_start();
 session_start();
 
 // DB Params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'twitter_db');   // Docker MySQL service name
+define('DB_USER', 'twitteruser');
+define('DB_PASS', 'twitterpass');
 define('DB_NAME', 'twitter');
 
 // App Root 
 define('APPROOT', dirname(dirname(__FILE__)));
+
 // URL Root
-define('URLROOT', 'http://localhost/twitter');
+define('URLROOT', 'http://localhost:8080');
+
 // Site Name
-define('SITENAME', 'Twitter');
+define('SITENAME', 'Twitter'); 
